3c183bfd0763dcc64652756370802150058d24b0
Gonzalo Odiard <godiard@gmail.com>
2016-05-29 23:17:15 -0300

    Add executeQuery to get-by-query collector script

--- scripts/get-by-query.sh
+++ scripts/get-by-query.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 export GET_BY_QUERY=$(grep -r getByQuery * --exclude=*.class | grep -v WithParams | wc -l)
 export GET_UNIQUE_BY_QUERY=$(grep -r getUniqueByQuery * --exclude=*.class | grep -v WithParams | wc -l)
+export EXECUTE_QUERY=$(grep -r Db.executeQuery * --exclude=*.class | grep -v WithParams | wc -l)
 export GET_BY_QUERY_WITH_PARAMS=$(grep -r getByQueryWithParams * --exclude=*.class | wc -l)
 export GET_UNIQUE_BY_QUERY_WITH_PARAMS=$(grep -r getUniqueByQueryWithParams * --exclude=*.class | wc -l)
-echo $GET_BY_QUERY $GET_UNIQUE_BY_QUERY $GET_BY_QUERY_WITH_PARAMS $GET_UNIQUE_BY_QUERY_WITH_PARAMS
+export EXECUTE_QUERY_WITH_PARAMS=$(grep -r executeQueryWithParams * --exclude=*.class | wc -l)
+echo $GET_BY_QUERY $GET_UNIQUE_BY_QUERY $EXECUTE_QUERY $GET_BY_QUERY_WITH_PARAMS $GET_UNIQUE_BY_QUERY_WITH_PARAMS $EXECUTE_QUERY_WITH_PARAMS
