79b15207d8358112df9778bd9f04d256b65f2014
Gonzalo Odiard <godiard@gmail.com>
2016-09-16 19:43:29 -0300

    Revert "Add script to monitor transition to DispatchAction"
    This reverts commit c77a07d340ba0d305093b391e0be9e59557277a1.

--- scripts/action.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-export DISPATCH=$(grep -r DispatchAction --include=*.java | grep -v Lookup | grep -v import | wc -l)
-export LOOKUP=$(grep -r LookupDispatchAction --include=*.java | grep -v import | wc -l)
-echo $DISPATCH $LOOKUP
